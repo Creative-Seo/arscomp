0715e63eefe6cc675b4f478079fb8dcd1f2d1947
ilcomp <cgde88@gmail.com>
2015-10-09 17:59:46 +0300

    u

--- dev/novosti/content.php
+++ dev/novosti/content.php
@@ -1,11 +1,15 @@
 <div class="container">
 <?php foreach ($novosti as $row) { ?>
 	<div class="row margin-bottom shadow">
-		<div class="col-sm-4 col-md-3 col-lg-2"><a href="<?php echo $row['url'];?>"><img src="/novosti/<?php echo $row['url'];?>/glav.jpg" class="img-responsive" alt="<?php echo $row['h2'];?>"></a></div>
+		<div class="col-sm-4 col-md-3 col-lg-2">
+			<a href="/novosti/<?php echo $row['url'];?>">
+				<img src="/novosti/<?php echo $row['url'];?>/glav.jpg" class="img-responsive" alt="<?php echo $row['h2'];?>">
+			</a>
+		</div>
 		<div class="col-sm-8 col-md-9 col-lg-10">
 			<h2><?php echo $row['h2'];?></h2>
 			<p><?php echo $row['p'];?></p>
-			<p><a href="<?php echo $row['url'];?>">Продолжить чтение...</a></p>
+			<p><a href="/novosti/<?php echo $row['url'];?>">Продолжить чтение...</a></p>
 		</div>
 	</div>
 <?php } ?>
